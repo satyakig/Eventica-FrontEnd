3d8f6ec65982e005222762cf76656cd6ccf5e5dd
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-27 19:43:07 -0700

    Fixing eslint

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,40 @@
 const functions = require('firebase-functions');
 const { v1 } = require('@google-cloud/firestore');
 const admin = require('firebase-admin');
+const moment = require('moment');
+
+admin.initializeApp();
 
 const client = new v1.FirestoreAdminClient();
-admin.initializeApp();
 const firestore = admin.firestore();
 
-const bucket = 'gs://seng-513.appspot.com/backups';
+const BACKUP_BUCKET = 'gs://seng-513.appspot.com/backups';
+const PROJECT_ID = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
 
 exports.databaseBackup = functions.pubsub.schedule('every 24 hours').onRun((context) => {
-  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-  const databaseName = client.databasePath(projectId, '(default)');
+  const databaseName = client.databasePath(PROJECT_ID, '(default)');
+  const today = moment().format('DD-MM-YYYY');
 
   return client
     .exportDocuments({
       name: databaseName,
-      outputUriPrefix: bucket,
+      outputUriPrefix: `${BACKUP_BUCKET}/${today}`,
       collectionIds: [],
     })
     .then((responses) => {
       const response = responses[0];
-      console.log(`Operation Name: ${response['name']}`);
+      console.debug(`Operation Name: ${response['name']}`);
       return response;
-    })
-    .catch((err) => {
-      console.error(err);
-      throw new Error('Export operation failed');
     });
 });
 
 exports.addUser = functions.auth.user().onCreate((user) => {
-  const defaultPicture = "https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg";
+  const defaultPicture =
+    'https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg';
 
   const id = user.uid;
   const name = user.displayName;
-  const userPhoto = user.photoURL ? user.photoURL: defaultPicture;
+  const userPhoto = user.photoURL ? user.photoURL : defaultPicture;
   const email = user.email;
   const phone = user.phoneNumber;
 
