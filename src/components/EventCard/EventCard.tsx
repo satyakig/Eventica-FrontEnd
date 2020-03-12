4a15f962572fe25ebee30c8d8e179abd1431825d
Satyaki Ghosh <satyakig901@hotmail.com>
2020-03-12 10:54:46 -0600

    Fixing card styles

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -5,24 +5,41 @@ import { eventCardStyles } from './EventCard.styles';
 
 const EventCard = (props: any) => {
   const classes = eventCardStyles();
+
+  function cardClick(): void {
+    console.log(props);
+  }
+
   return (
-    <Card key={props.index} className={classes.cardContainer}>
-      <CardActionArea className={classes.cardClick}>
+    <Card className={classes.cardContainer}>
+      <CardActionArea className={classes.cardClick} onClick={cardClick}>
         <CardMedia
+          className={classes.cardMedia}
           component="img"
           alt={props.event.name}
-          height="140"
           image={props.event.photoURL}
           title={props.event.name}
         />
-        <CardContent>
-          <Typography className={classes.subtitle} color="textSecondary" gutterBottom>
+        <CardContent className={classes.cardContent}>
+          <Typography className={classes.subtitle} color="textSecondary" gutterBottom={true}>
             {moment(props.event.start).format('ddd, MMM D, hh:mm A')}
           </Typography>
-          <Typography className={classes.title} color="textPrimary" variant="h5" component="h2">
+          <Typography
+            className={classes.title}
+            color="textPrimary"
+            variant="h5"
+            component="h2"
+            gutterBottom={true}
+          >
             {props.event.name}
           </Typography>
-          <Typography color="textSecondary" variant="body2" component="p" noWrap={false}>
+          <Typography
+            className={classes.desc}
+            color="textSecondary"
+            variant="body2"
+            component="p"
+            noWrap={false}
+          >
             {props.event.desc}
           </Typography>
         </CardContent>
--- src/components/EventGrid/EventGrid.tsx
+++ src/components/EventGrid/EventGrid.tsx
@@ -4,18 +4,18 @@ import { useSelector } from 'react-redux';
 import { ReduxState } from 'redux/combinedReducer';
 import EventCard from '../EventCard/EventCard';
 
-const EventGrid = (props: any) => {
+const EventGrid = () => {
   const events = useSelector((state: ReduxState) => {
     return state.events.events.getAllData();
   });
 
   return (
     <Container maxWidth="xl">
-      <Grid container spacing={3}>
+      <Grid container={true} spacing={3}>
         {events.map((event, index) => {
           return (
-            <Grid item xs={12} sm={3}>
-              <EventCard event={event} index={index} />
+            <Grid item={true} key={index} xs={12} sm={6} md={4} lg={3}>
+              <EventCard event={event} />
             </Grid>
           );
         })}
