7c3920a4d81207426404a59b3161434f5fd1b800
Andy Yang <andyyang350@gmail.com>
2020-03-14 18:55:08 -0600

    finish create modal ui

--- src/components/CreateEvent/CreateEvent.tsx
+++ src/components/CreateEvent/CreateEvent.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
+import { useSelector } from 'react-redux';
+import { ReduxState } from 'redux/combinedReducer';
+import moment from 'moment-timezone';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
-import Input from '@material-ui/core/Input';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import {
   Container,
   FormControl,
@@ -16,10 +13,19 @@ import {
   OutlinedInput,
   DialogTitle,
   DialogActions,
+  TextField,
+  Button,
+  Dialog,
+  Input,
+  Typography,
+  InputAdornment,
+  Checkbox,
+  IconButton,
 } from '@material-ui/core';
-import MomentUtils from '@date-io/moment';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
+import CloseIcon from '@material-ui/icons/Close';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 
 type CreateEventProps = {
   openCreateEvent: boolean;
@@ -41,26 +47,80 @@ export const profileStyles = makeStyles((theme: Theme) => {
     gridItem: {
       padding: '12px',
     },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
   });
 });
 
 export default function CreateEvent(props: CreateEventProps) {
   const classes = profileStyles();
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const emptyArray: string[] = [];
   const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [eventType, setEventType] = useState(0);
+  const [startDate, setStartDate] = useState(moment().valueOf());
+  const [endDate, setEndDate] = useState(moment().valueOf());
+  const [location, setLocation] = useState('');
+  const [categories, setCategories] = useState(emptyArray);
+  const [eventPicture, setEventPicture] = useState('');
+  const [amount, setAmount] = useState('');
+  const [capacity, setCapacity] = useState(0);
+
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+  const allEventTypes = [
+    { label: 'Public', value: 0 },
+    { label: 'Private', value: 1 },
+  ];
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
-  const options = [{ value: 'Concert' }, { value: 'strawberry' }, { value: 'vanilla' }];
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(event.target.value);
+  };
+
+  const handleCategoryChange = (values: string[]) => {
+    setCategories(values);
+  };
+
+  const handleEventTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEventType(parseInt(event.target.value));
+  };
+
+  const handleCapacityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCapacity(parseInt(event.target.value));
+  };
+
+  const handlePictureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEventPicture(event.target.value);
+  };
+
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
+
+  const allEventCategories = useSelector((state: ReduxState) => {
+    return state.appState.categoriesArray;
+  });
+
   return (
     <Dialog
       onClose={props.handleClose}
       aria-labelledby="customized-dialog-title"
       open={props.openCreateEvent}
-      maxWidth="sm"
+      maxWidth="md"
       fullWidth={true}
     >
       <Container maxWidth="lg">
@@ -95,22 +155,75 @@ export default function CreateEvent(props: CreateEventProps) {
             </Grid>
 
             <Grid item={true} className={classes.gridItem}>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel htmlFor="component-outlined">Description</InputLabel>
+                <OutlinedInput
+                  id="component-outlined-name"
+                  value={description}
+                  onChange={handleDescriptionChange}
+                  label="Description"
+                  fullWidth
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item={true} className={classes.gridItem}>
+              <TextField
+                fullWidth
+                select
+                label="Event Type"
+                value={eventType}
+                onChange={handleEventTypeChange}
+                SelectProps={{
+                  native: true,
+                }}
+                variant="outlined"
+              >
+                {allEventTypes.map((option) => {
+                  return (
+                    <option key={option.label} value={option.value}>
+                      {option.label}
+                    </option>
+                  );
+                })}
+              </TextField>
+            </Grid>
+
+            <Grid item={true} className={classes.gridItem}>
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel htmlFor="component-outlined">Max Capacity</InputLabel>
+                <OutlinedInput
+                  type="number"
+                  id="component-outlined-name"
+                  value={capacity}
+                  onChange={handleCapacityChange}
+                  label="Max Capacity"
+                  fullWidth
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item={true} className={classes.gridItem}>
+              <MuiPickersUtilsProvider utils={MomentUtils}>
                 <DateTimePicker
                   style={{ marginRight: 20 }}
                   inputVariant="outlined"
-                  value={startDate}
+                  value={moment(startDate)}
                   disablePast
-                  onChange={(date) => {}}
+                  onChange={(date) => {
+                    setStartDate(date ? date.valueOf() : moment().valueOf());
+                  }}
                   label="Start"
                   showTodayButton
                 />
 
                 <DateTimePicker
                   inputVariant="outlined"
-                  value={endDate}
+                  value={moment(endDate)}
                   disablePast
-                  onChange={(date) => {}}
+                  onChange={(date) => {
+                    setEndDate(date ? date.valueOf() : moment().valueOf());
+                  }}
                   label="End"
                   showTodayButton
                 />
@@ -122,8 +235,8 @@ export default function CreateEvent(props: CreateEventProps) {
                 <InputLabel htmlFor="component-outlined">Location</InputLabel>
                 <OutlinedInput
                   id="component-outlined-name"
-                  value={name}
-                  onChange={handleNameChange}
+                  value={location}
+                  onChange={handleLocationChange}
                   label="Location"
                   fullWidth
                 />
@@ -131,39 +244,62 @@ export default function CreateEvent(props: CreateEventProps) {
             </Grid>
 
             <Grid item={true} className={classes.gridItem}>
-              <TextField
-                fullWidth
-                select
-                label="Category"
-                value={() => {}}
-                onChange={() => {}}
-                SelectProps={{
-                  native: true,
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                id="tags-standard"
+                options={allEventCategories}
+                getOptionLabel={(option) => {
+                  return option;
                 }}
-                variant="outlined"
-              >
-                {options.map((option) => {
+                renderOption={(option, { selected }) => {
                   return (
-                    <option key={option.value} value={option.value}>
-                      {option.value}
-                    </option>
+                    <React.Fragment>
+                      <Checkbox
+                        icon={icon}
+                        checkedIcon={checkedIcon}
+                        style={{ marginRight: 8 }}
+                        checked={selected}
+                      />
+                      {option}
+                    </React.Fragment>
                   );
-                })}
-              </TextField>
+                }}
+                renderInput={(params) => {
+                  return (
+                    <TextField
+                      {...params}
+                      variant="standard"
+                      label="Categories"
+                      placeholder="Categories"
+                    />
+                  );
+                }}
+                onChange={(event, value) => {
+                  handleCategoryChange(value);
+                }}
+              />
             </Grid>
 
             <Grid item={true} className={classes.gridItem}>
-              <InputLabel htmlFor="standard-adornment-amount">Upload Picture</InputLabel>
-              <Input disableUnderline type="file" />
+              <InputLabel htmlFor="standard-adornment-amount">Upload Event Picture</InputLabel>
+              <Input
+                disableUnderline
+                type="file"
+                onChange={(event) => {
+                  console.log(event.target.value);
+                }}
+              />
             </Grid>
 
             <Grid item={true} className={classes.gridItem}>
               <FormControl variant="outlined" fullWidth>
                 <InputLabel htmlFor="component-outlined">Amount</InputLabel>
                 <OutlinedInput
+                  type="number"
                   id="component-outlined-name"
-                  value={name}
-                  onChange={handleNameChange}
+                  value={amount}
+                  onChange={handleAmountChange}
                   label="Amount"
                   fullWidth
                   startAdornment={<InputAdornment position="start">$</InputAdornment>}
@@ -174,8 +310,15 @@ export default function CreateEvent(props: CreateEventProps) {
 
           <Grid item={true} className={classes.gridItem}>
             <DialogActions>
-              <Button autoFocus onClick={props.handleClose} color="primary" variant="contained">
-                Submit
+              <Button
+                autoFocus
+                onClick={() => {
+                  console.log(endDate);
+                }}
+                color="primary"
+                variant="contained"
+              >
+                Create Event
               </Button>
             </DialogActions>
           </Grid>
